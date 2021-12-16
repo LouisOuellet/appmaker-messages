460c679d07259f69fef3c45e342d0f9d49a5890a
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-16 11:40:21 -0500

    21.12-dev-56

--- cron.php
+++ cron.php
@@ -4,6 +4,6 @@ require_once dirname(__FILE__,3).'/plugins/messages/api.php';
 
 $API = new messagesAPI();
 
-$cron = $API->getMail("webman-01.albcie.com","993","SSL","patch@albcustoms.com","qqgScjZ@AK6S");
+$cron = $API->getMail("webman-02.albcie.com","993","SSL","customs@albcie.com","oBRYU!xchoh3A");
 
 if((isset($this->Settings['debug']))&&($this->Settings['debug'])&&($cron!=null)){ echo json_encode($cron, JSON_PRETTY_PRINT); }
