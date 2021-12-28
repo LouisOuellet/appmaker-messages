7b9b48e9b5158a63f029ff1f4a6c657b72915441
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-28 10:38:13 -0500

    21.12-dev-94

--- dist/js/script.js
+++ dist/js/script.js
@@ -50,9 +50,9 @@ API.Plugins.messages = {
 				var checkExist = setInterval(function() {
 					if(layout.timeline.find('div.time-label[data-dateus="'+dateUS+'"]').length > 0){
 						clearInterval(checkExist);
-						console.log(layout.timeline.lenght);
+						console.log(API.Helper.isSet(layout,['timeline']));
 						console.log(layout.timeline.find('.time-label').first().find('div.btn-group').find('button[data-trigger="messages"]').length);
-						if(layout.timeline.lenght > 0 && layout.timeline.find('.time-label').first().find('div.btn-group').find('button[data-trigger="messages"]').length <= 0){
+						if(API.Helper.isSet(layout,['timeline']) && layout.timeline.find('.time-label').first().find('div.btn-group').find('button[data-trigger="messages"]').length <= 0){
 							layout.timeline.find('.time-label').first().find('div.btn-group').append('<button class="btn btn-secondary" data-trigger="messages">'+API.Contents.Language['Messages']+'</button>');
 						}
 						var html = '';
