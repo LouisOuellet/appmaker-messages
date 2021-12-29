eb02b8380f30caf37a40bda33219ee7c40311474
Louis Ouellet <louis@TheMacSwitch.local>
2021-12-29 15:30:04 -0500

    21.12-dev-107

--- cron.php
+++ cron.php
@@ -4,6 +4,9 @@ require_once dirname(__FILE__,3).'/plugins/messages/api.php';
 
 $API = new messagesAPI();
 
-$cron = $API->getMail("webman-02.albcie.com","993","SSL","customs@albcie.com","oBRYU!xchoh3A");
-
-if((isset($this->Settings['debug']))&&($this->Settings['debug'])&&($cron!=null)){ echo json_encode($cron, JSON_PRETTY_PRINT); }
+if(isset($this->Settings['plugins']['messages']['settings']['accounts']) && is_array($this->Settings['plugins']['messages']['settings']['accounts']) && !empty($this->Settings['plugins']['messages']['settings']['accounts'])){
+  foreach($this->Settings['plugins']['messages']['settings']['accounts'] as $account){
+    if(isset($this->Settings['debug']) && $this->Settings['debug']){ echo "[".$fileID."]File saved\n"; }
+    $cron = $API->getMail($account['host'],$account['port'],$account['encryption'],$account['username'],$account['password']);
+  }
+}
