c44d4887e80adf6be2ce78f59af5307650483b2d
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 14:41:08 -0500

    22.01-dev-112

--- api.php
+++ api.php
@@ -1,22 +1,22 @@
 <?php
 class messagesAPI extends CRUDAPI {
 
-	public function buildRelation($relations,$relation){
-		$relationships = $this->getRelationships($relation['relationship'],$relation['link_to']);
-		foreach($relationships as $id => $links){
-			foreach($links as $details){
-				if(isset($this->Settings['plugins'][$details['relationship']]['status']) && $this->Settings['plugins'][$details['relationship']]['status']){
-					$recordDetail = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
-					if($recordDetail->numRows() > 0){
-						$recordDetail = $recordDetail->fetchAll()->All()[0];
-						if($details['relationship'] == 'files'){ unset($recordDetail['file']); }
-						$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
-					}
-				}
-			}
-		}
-    return $relations;
-  }
+	// public function buildRelation($relations,$relation){
+	// 	$relationships = $this->getRelationships($relation['relationship'],$relation['link_to']);
+	// 	foreach($relationships as $id => $links){
+	// 		foreach($links as $details){
+	// 			if(isset($this->Settings['plugins'][$details['relationship']]['status']) && $this->Settings['plugins'][$details['relationship']]['status']){
+	// 				$recordDetail = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
+	// 				if($recordDetail->numRows() > 0){
+	// 					$recordDetail = $recordDetail->fetchAll()->All()[0];
+	// 					if($details['relationship'] == 'files'){ unset($recordDetail['file']); }
+	// 					$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+  //   return $relations;
+  // }
 
 	public function read($request = null, $data = null){
 		if(($data != null)||($data == null)){
