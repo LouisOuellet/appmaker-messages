010e210e2bef02bb260d5386a71c2e4b4b37a935
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 14:58:10 -0500

    22.01-dev-114

--- api.php
+++ api.php
@@ -5,12 +5,14 @@ class messagesAPI extends CRUDAPI {
 		$relationships = $this->getRelationships($relation['relationship'],$relation['link_to']);
 		foreach($relationships as $id => $links){
 			foreach($links as $details){
-				if(isset($this->Settings['plugins'][$details['relationship']]['status']) && $this->Settings['plugins'][$details['relationship']]['status']){
-					$recordDetail = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
-					if($recordDetail->numRows() > 0){
-						$recordDetail = $recordDetail->fetchAll()->All()[0];
-						if($details['relationship'] == 'files'){ unset($recordDetail['file']); }
-						$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
+				if(in_array($details['relationship'],['files','contacts'])){
+					if(isset($this->Settings['plugins'][$details['relationship']]['status']) && $this->Settings['plugins'][$details['relationship']]['status']){
+						$recordDetail = $this->Auth->query('SELECT * FROM `'.$details['relationship'].'` WHERE `id` = ?',$details['link_to']);
+						if($recordDetail->numRows() > 0){
+							$recordDetail = $recordDetail->fetchAll()->All()[0];
+							if($details['relationship'] == 'files'){ unset($recordDetail['file']); }
+							$relations[$relation['relationship']][$relation['link_to']][$details['relationship']][$recordDetail['id']] = $recordDetail;
+						}
 					}
 				}
 			}
