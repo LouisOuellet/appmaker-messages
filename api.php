cab4d78c618ae97721d18048bffc3f527f19fcfd
Louis Ouellet <louis@TheMacSwitch.local>
2022-02-09 11:21:02 -0500

    22.02-dev-198

--- api.php
+++ api.php
@@ -137,20 +137,20 @@ class messagesAPI extends CRUDAPI {
 			libxml_use_internal_errors(false);
 			$images = $document->getElementsByTagName('img');
 			$a = $document->createElement('a');
-			// foreach($images as $key => $image){
-			// 	$src['old'] = $image->getAttribute('src');
-			// 	$src['new'] = 'plugins/messages/dist/img/image-not-found.png';
-			// 	if(isset($this->Settings['plugins']['files']['status']) && $this->Settings['plugins']['files']['status']){
-			// 		$file = $this->Helper->files->cache($files[$key]);
-			// 		if($file){ $src['new'] = $file; }
-			// 	}
-			// 	$image->setAttribute('src', $src['new']);
-			// 	$image->setAttribute('data-src', $src['old']);
-			// 	$image->addStyle('max-width:', '500px;');
-			// 	$node = $a->cloneNode();
-			// 	$image->parentNode->replaceChild($node,$image);
-			// 	$node->appendChild($image);
-			// }
+			foreach($images as $key => $image){
+				$src['old'] = $image->getAttribute('src');
+				$src['new'] = 'plugins/messages/dist/img/image-not-found.png';
+				// if(isset($this->Settings['plugins']['files']['status']) && $this->Settings['plugins']['files']['status']){
+				// 	$file = $this->Helper->files->cache($files[$key]);
+				// 	if($file){ $src['new'] = $file; }
+				// }
+				// $image->setAttribute('src', $src['new']);
+				// $image->setAttribute('data-src', $src['old']);
+				// $image->addStyle('max-width:', '500px;');
+				// $node = $a->cloneNode();
+				// $image->parentNode->replaceChild($node,$image);
+				// $node->appendChild($image);
+			}
 			// return $document->saveHTML();
 			return $html;
 		} else { return $html; }
